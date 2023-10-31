64497eaf44c7029ce4862c3a41a51d8efde373e7
hojicya <shimi@uni.local>
2023-10-31 13:24:19 +0900

    Add plugin fonts

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,13 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
+    VitePluginFonts({
+      google: {
+        families: [
+          'Poppins'
+        ],
+      }
+    })
   ],
   resolve: {
     alias: {
