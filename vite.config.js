eab923fc59454d7f27552f68e691641fc285d029
hojicya <shimi@uni.local>
2023-11-09 15:11:45 +0900

    Fix VitePluginFonts loading error

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePluginFonts } from "vite-plugin-fonts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
