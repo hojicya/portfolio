abb589dd4921d167e3cf965f7ae6f2152f841b8c
hojicya <shimi@uni.local>
2023-11-10 15:10:43 +0900

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <MyNavigation />
   <RouterView />
+  <MyFooter />
 </template>
 
 <script>
 import { RouterLink, RouterView } from "vue-router";
 import MyNavigation from "./components/organisms/TheNavigation.vue";
+import MyFooter from "./components/organisms/TheFooter.vue";
 
 export default {
   components: {
     MyNavigation,
+    MyFooter,
   },
 };
 </script>
---
+++ src/components/organisms/TheFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="footer">
+    <p>&copy; 2023 Miyuki Shimura</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  text-align: right;
+  padding: 60px 100px;
+  font-size: 14px;
+}
+</style>
