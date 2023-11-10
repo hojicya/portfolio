d96077307cee33a870b0448b755e75447d4b85bd
hojicya <shimi@uni.local>
2023-11-10 13:26:17 +0900

    Remove unnecessary files

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Top from "../pages/index.vue";
-import About from "../pages/about.vue";
-import Works from "../pages/works.vue";
+import Index from "../pages/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "top",
-      component: Top,
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: About,
-    },
-    {
-      path: "/works",
-      name: "works",
-      component: Works,
+      name: "index",
+      component: Index,
     },
   ],
 });
